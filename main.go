9372be089ea690304790725d1cbbdb447255bccb
aman.gandhi <aman.gandhi@u-next.com>
2025-05-28 19:24:57 +0530

    add events routes to save and get events + DB connection and create table

--- main.go
+++ main.go
@@ -1,13 +1,32 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/http"
+
+	"github.com/Aman5681/auth-micro/db"
+	"github.com/Aman5681/auth-micro/models"
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
-	router := gin.Default()
-	router.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
-	router.Run() // listen and serve on 0.0.0.0:8080
+	db.InitDb()
+	server := gin.Default()
+	server.GET("/events", getEvents)
+	server.POST("/events", createEvent)
+	server.Run(":8080") // listen and serve on 0.0.0.0:8080
+}
+
+func getEvents(context *gin.Context) {
+	events := models.GetALLEvents()
+	context.JSON(http.StatusOK, events)
+}
+
+func createEvent(context *gin.Context) {
+	var event models.Event
+	err := context.ShouldBindJSON(&event)
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	event.Save(context)
 }
